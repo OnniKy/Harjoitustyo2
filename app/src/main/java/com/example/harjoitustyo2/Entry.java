3e728f0a38ab6d463119686db459ced5a31f2141
tomiruohonen <tomi.ruohonen@hotmail.com>
2021-03-22 11:32:01 +0200

    Entry luokka tehty

---
+++ app/src/main/java/com/example/harjoitustyo2/Entry.java
@@ -0,0 +1,29 @@
+package com.example.harjoitustyo2;
+
+import java.util.Date;
+
+public class Entry {
+    Date date;
+
+    public Entry(){
+
+    }
+
+    public class ClimateDietEntry(){
+        float dairyConsumption;
+        float meatConsumption;
+
+        public float useFoodcalculatorApi(){
+
+        }
+
+    }
+
+    public class WeightEntry(){
+        int weight;
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+    }
+}
