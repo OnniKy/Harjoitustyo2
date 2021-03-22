4cc20f134cfa5294410d8ec820668af540f1ca79
tomiruohonen <tomi.ruohonen@hotmail.com>
2021-03-22 11:35:13 +0200

    EntryManager luokka tehty

---
+++ app/src/main/java/com/example/harjoitustyo2/EntryManager.java
@@ -0,0 +1,21 @@
+package com.example.harjoitustyo2;
+
+public class EntryManager {
+    Entry entry;
+
+    public EntryManager(){
+
+    }
+
+    public boolean SaveEntries(Entry e){
+
+    }
+
+    public Entry getEntry(int s) {
+        return entry;
+    }
+
+    public boolean readEntries(){
+
+    }
+}
