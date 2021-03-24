40f8f8034aaa4db8d26d569afcb709d5fe706533
onnikyrkko <onni.kyrkko@gmail.com>
2021-03-24 11:22:59 +0200

    rekisteröintiä

--- app/src/main/java/com/example/harjoitustyo2/RegisterActivity.java
+++ app/src/main/java/com/example/harjoitustyo2/RegisterActivity.java
@@ -3,16 +3,24 @@ package com.example.harjoitustyo2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity{
 
     UserManager userManager = null;
+    EditText name, email, password, municipality, height, weight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        name=findViewById(R.id.editName);
+        email=findViewById(R.id.editEmail);
+        password=findViewById(R.id.editPassword);
+        municipality=findViewById(R.id.editMunicipality);
+        height=findViewById(R.id.editHeight);
+        weight=findViewById(R.id.editWeight);
 
         userManager = (UserManager) getIntent().getSerializableExtra("UserManager");
 
