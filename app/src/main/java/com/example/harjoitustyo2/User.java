1ba13dfcfe10adac8c7cd4af0edcd4bac38a7e3e
tomiruohonen <tomi.ruohonen@hotmail.com>
2021-03-24 11:10:02 +0200

    Kirjautumista

--- app/src/main/java/com/example/harjoitustyo2/User.java
+++ app/src/main/java/com/example/harjoitustyo2/User.java
@@ -13,19 +13,21 @@ public class User {
     String username;
     String gender;
 
-    public User(int height, double weight, Date birthYear, String municipality, double bmi, String username, String gender){
+    public User(int height, double weight, Date birthYear, String municipality, String username, String gender){
         this.height = height;
         this.weight = weight;
         this.birthYear = birthYear;
         this.municipality = municipality;
-        this.bmi = bmi;
+        this.calulateBmi(this.height, this.weight);
         this.username = username;
         this.gender = gender;
+
 
     }
 
     private void calulateBmi(int height, double weight){
 
+        this.bmi = bmi;
     }
 
     public void updateWeight(){
