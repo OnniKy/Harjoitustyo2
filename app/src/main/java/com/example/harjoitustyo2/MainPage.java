e5f1829008e37a21de1ef8e0f722a119606388d2
onnikyrkko <onni.kyrkko@gmail.com>
2021-03-30 16:09:17 +0300

    main page

--- app/src/main/java/com/example/harjoitustyo2/MainPage.java
+++ app/src/main/java/com/example/harjoitustyo2/MainPage.java
@@ -3,12 +3,18 @@ package com.example.harjoitustyo2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainPage extends AppCompatActivity {
+
+    EditText dailyWeight, dailyClimate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        dailyWeight = findViewById(R.id.dailyWeight);
+        dailyClimate = findViewById(R.id.dailyClimate);
     }
 }
