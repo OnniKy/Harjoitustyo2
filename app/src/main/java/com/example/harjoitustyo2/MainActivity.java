03b3f11e2b7fa3539f4bd0852573fa58d6bf5650
tomiruohonen <tomi.ruohonen@hotmail.com>
2021-03-30 15:41:44 +0300

    Kirjautumista

--- app/src/main/java/com/example/harjoitustyo2/MainActivity.java
+++ app/src/main/java/com/example/harjoitustyo2/MainActivity.java
@@ -6,13 +6,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
     TextView password;
-    TextView address;
+    TextView username;
     UserManager userManager;
+    Button register, login;
 
 
     @Override
@@ -21,25 +23,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         context = MainActivity.this;
 
-        address = (TextView) findViewById(R.id.address);
+        username = (TextView) findViewById(R.id.address);
         password = (TextView) findViewById(R.id.password);
 
         userManager = UserManager.getInstance(context);
 
         System.out.println("Sijainti kirjastossa: " + context.getFilesDir());
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userManager.checkUsername(username.getText().toString());
+                userManager.checkPassword();
+            }
+        });
+
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                intent.putExtra("UserManager", userManager);
+                startActivityForResult(intent, 1);
+            }
+        });
+
     }
 
-    public void signIn(View v){
-        userManager.checkUsername(address.getText().toString());
-        userManager.checkPassword();
-    }
-
-    public void register(View v){
-        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-        intent.putExtra("UserManager", userManager);
-        startActivityForResult(intent, 1);
-
-    }
 
 }
