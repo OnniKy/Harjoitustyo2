dec2b6b041faff3eac515823f1356aa4d97070bc
onnikyrkko <onni.kyrkko@gmail.com>
2021-03-31 10:46:19 +0300

    weigth Control

--- app/src/main/java/com/example/harjoitustyo2/WeightControl.java
+++ app/src/main/java/com/example/harjoitustyo2/WeightControl.java
@@ -3,12 +3,17 @@ package com.example.harjoitustyo2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class WeightControl extends AppCompatActivity {
+
+    EditText addWeight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight_control);
+        addWeight = findViewById(R.id.dailyWeightButton);
     }
 }
