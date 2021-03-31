871a02ef5195cd14c7fe410b379192d3c28ae09a
linuspeltonen <linus.peltonen@student.lut.fi>
2021-03-31 14:37:38 +0300

    CLimate control updates + spinner

--- app/src/main/java/com/example/harjoitustyo2/ClimateControl.java
+++ app/src/main/java/com/example/harjoitustyo2/ClimateControl.java
@@ -10,5 +10,6 @@ public class ClimateControl extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_climate_control);
+
     }
 }
