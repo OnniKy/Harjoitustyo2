37256f18cbb6b7c8a6b796c9d9dad13a375e5e46
tomiruohonen <tomi.ruohonen@hotmail.com>
2021-03-31 15:25:53 +0300

    API Toimii

--- app/src/main/java/com/example/harjoitustyo2/ClimateControl.java
+++ app/src/main/java/com/example/harjoitustyo2/ClimateControl.java
@@ -2,14 +2,43 @@ package com.example.harjoitustyo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
 
 public class ClimateControl extends AppCompatActivity {
+    JSONRequest jsonRequest;
+    Button submit;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_climate_control);
+        context = ClimateControl.this;
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
+        jsonRequest = new JSONRequest();
+        submit = findViewById(R.id.button2);
+
+        submit.setOnClickListener(v -> {
+            String diet = "omnivore";
+            String beef = "2";
+            String fish = "1";
+            String pork = "5";
+            String dairy = "3";
+            String cheese = "5";
+            String rice = "2";
+            String egg = "0";
+            String salad = "4";
+
+            jsonRequest.readJSON(diet, beef, fish, pork, dairy, cheese, rice, egg, salad);
+        });
 
     }
 }
--- app/src/main/java/com/example/harjoitustyo2/JSONRequest.java
+++ app/src/main/java/com/example/harjoitustyo2/JSONRequest.java
@@ -1,5 +1,8 @@
 package com.example.harjoitustyo2;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,25 +13,45 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 
+
 public class JSONRequest {
 
-    public void readJSON(String diet, String beef, String fish, String pork, String dairy, String cheese, String rice, String egg, String salad, String restaurant){
-        String json = getJSON(diet, beef, fish, pork, dairy, cheese, rice, egg, salad, restaurant);
+    public void readJSON(String diet, String beef, String fish, String pork, String dairy, String cheese, String rice, String egg, String salad){
+        String json = getJSON(diet, beef, fish, pork, dairy, cheese, rice, egg, salad);
+        System.out.println("TÄHÄN TOIMII");
+        if (json != null){
+            try {
+                JSONArray jsonArray = new JSONArray(json);
+                for (int i = 0; i < jsonArray.length() ; i++){
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    System.out.println("********" + (i+1) + "*********");
+                    System.out.println(jsonObject.getString("Dairy"));
+                    System.out.println(jsonObject.getString("Meat"));
+                    System.out.println(jsonObject.getString("Plant"));
+                    System.out.println(jsonObject.getString("Total"));
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("It works");
+            }
+        }
 
     }
 
 
     // Uses Climate Diet API
-    public String getJSON(String diet, String bLevel, String fLevel, String pLevel, String dLevel, String cLevel, String rLevel, String eLevel, String sLevel, String rSpendind){
+    public String getJSON(String diet, String bLevel, String fLevel, String pLevel, String dLevel, String cLevel, String rLevel, String eLevel, String sLevel){
         String response = null;
         try {
-            URL url = new URL("https://ilmastodieetti.ymparisto.fi/ilmastodieetti/calculatorapi/v1/FoodCalculator?query.diet=" + diet + "&query.beefLevel=" + bLevel + "&query.fishLevel=" + fLevel + "&query.porkPoultryLevel=" + pLevel + "&query.dairyLevel=" + dLevel + "&query.cheeseLevel=" + cLevel + "&query.riceLevel=" + rLevel + "&query.eggLevel=" + eLevel + "&query.winterSaladLevel=+" + sLevel + "&query.restaurantSpending=" + rSpendind);
+            URL url = new URL("https://ilmastodieetti.ymparisto.fi/ilmastodieetti/calculatorapi/v1/FoodCalculator?query.diet=" + diet + "&query.beefLevel=" + bLevel + "&query.fishLevel=" + fLevel + "&query.porkPoultryLevel=" + pLevel + "&query.dairyLevel=" + dLevel + "&query.cheeseLevel=" + cLevel + "&query.riceLevel=" + rLevel + "&query.eggLevel=" + eLevel + "&query.winterSaladLevel=+" + sLevel);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             InputStream in = new BufferedInputStream(conn.getInputStream());
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             StringBuilder sb = new StringBuilder();
-            String line = null;
+            String line;
 
             while ((line = br.readLine()) != null) {
                 sb.append(line).append("\n");
@@ -47,6 +70,7 @@ public class JSONRequest {
             System.out.println("***GETJSON TOIMII***");
         }
 
+        System.out.println(response);
         return response;
     }
 }
