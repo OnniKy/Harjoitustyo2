b2f6a8c83fdff28f65dc659f73c66059522915b6
onnikyrkko <onni.kyrkko@gmail.com>
2021-04-06 14:11:51 +0300

    weigth Control

--- app/src/main/java/com/example/harjoitustyo2/JSONRequest.java
+++ app/src/main/java/com/example/harjoitustyo2/JSONRequest.java
@@ -1,10 +1,16 @@
 package com.example.harjoitustyo2;
 
+import android.view.View;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -74,6 +80,79 @@ public class JSONRequest {
         }
 
         return response;
+    }
+
+    public void writeFile(View v){
+        String weight = addWeight.getText().toString();
+        File file = new File(context.getFilesDir(), FILE_NAME);
+
+        if(!file.exists()){
+            try{
+                file.createNewFile();
+                fileWriter = new FileWriter(file.getAbsoluteFile());
+                bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write("{}");
+                bufferedWriter.close();
+
+
+            }catch (IOException e){
+                e.printStackTrace();
+
+            }
+        }
+        try {
+            StringBuffer output = new StringBuffer();
+
+            fileReader = new FileReader(file.getAbsoluteFile());
+            bufferedReader = new BufferedReader(fileReader);
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+            response = output.toString();
+            bufferedReader.close();
+
+
+            messageDetails = new JSONObject(response);
+            isUserExisting = messageDetails.has("Weight");
+            if (!isUserExisting) {
+                JSONArray newUserMessages = new JSONArray();
+                newUserMessages.put(weight);
+                messageDetails.put("Weight", newUserMessages);
+            } else {
+                JSONArray userMessages = (JSONArray) messageDetails.get("Weight");
+                userMessages.put(weight);
+            }
+
+            fileWriter = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+            bw.write(messageDetails.toString());
+            bw.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void lue(View v) throws Exception
+    {
+        File file = new File(context.getFilesDir(), FILE_NAME);
+        fileReader = new FileReader(file.getAbsoluteFile());
+        bufferedReader = new BufferedReader(fileReader);
+        String line = "";
+        while ((line = bufferedReader.readLine()) != null) {
+            String last5 = line.substring(line.length()-(line.length()-11));
+            String first = last5.substring(0, last5.length()-2);
+            String [] splitline = first.split(",");
+            String splitcut1 = splitline[splitline.length - 1].substring(0,splitline[splitline.length - 1].length()-1);
+            String splitcut2 = splitcut1.substring(splitcut1.length()-(splitcut1.length()-1));
+            System.out.println(splitcut2);
+
+
+        }
+
+        bufferedReader.close();
+
     }
 
 
