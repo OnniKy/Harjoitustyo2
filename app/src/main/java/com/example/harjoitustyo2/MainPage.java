fa34b040ac1948e915983c0fcb50c44b1ac168b0
tomiruohonen <tomi.ruohonen@hotmail.com>
2021-04-15 16:03:39 +0300

    Kofeiinicontrolli valmis & dashboardi tehty

--- app/src/main/java/com/example/harjoitustyo2/MainPage.java
+++ app/src/main/java/com/example/harjoitustyo2/MainPage.java
@@ -1,7 +1,9 @@
 package com.example.harjoitustyo2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,10 +17,10 @@ public class MainPage extends AppCompatActivity {
 
     JSONRequest jsonRequest;
     JSONFileControl jsonFileControl;
-    TextView totalEmission, dailyWeight, bmiTextbox;
-    Button climateButton, weightButton, caffeineButton;
+    TextView totalEmission, dailyWeight, bmiView, nameView, caffeineView, municipalityView;
+    CardView climateButton, weightButton, caffeineButton;
     ImageButton logOut;
-    String emission = null;
+    String emission = null, caffeine = null;
     String weight;
     Context context;
     String username, name;
@@ -27,19 +29,21 @@ public class MainPage extends AppCompatActivity {
     User user;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        dailyWeight = findViewById(R.id.dailyWeight);
-        bmiTextbox = findViewById(R.id.bmiTextbox);
-        climateButton = findViewById(R.id.toClimateControl);
-        weightButton = findViewById(R.id.toWeightControl);
-        caffeineButton = findViewById(R.id.toCaffeineControl);
+        dailyWeight = findViewById(R.id.weightView);
+        bmiView = findViewById(R.id.bmiView);
+        climateButton = findViewById(R.id.climateCard);
+        weightButton = findViewById(R.id.weightCard);
+        caffeineButton = findViewById(R.id.caffeineCard);
         logOut = findViewById(R.id.logOutButton);
+        nameView = findViewById(R.id.nameView);
+        caffeineView = findViewById(R.id.caffeineView);
+        municipalityView = findViewById(R.id.municipalityView);
 
-        totalEmission = findViewById(R.id.textView14);
+        totalEmission = findViewById(R.id.coEmission);
         context = MainPage.this;
 
         jsonRequest = new JSONRequest();
@@ -52,10 +56,14 @@ public class MainPage extends AppCompatActivity {
         name = user.getName();
 
 
+        nameView.setText(name);
+        municipalityView.setText(user.getMunicipality());
+
         try {
             weight = jsonFileControl.readLog(context, name, "Weight"); //TODO
-            dailyWeight.setText("Your weight is: " + weight + "kg");
+            dailyWeight.setText("Current weight: " + weight + "kg");
             emission = jsonFileControl.readLog(context, name, "Total");
+            caffeine = jsonFileControl.readLog(context, name, "Caffeine");
 
 
         } catch (Exception e) {
@@ -66,6 +74,12 @@ public class MainPage extends AppCompatActivity {
             totalEmission.setText("Total CO2 emission: " + modifyJSON(emission) + " kg per year");
         } else {
             totalEmission.setText("Calculate your emission on Climate \nControl page!");
+        }
+
+        if (caffeine != null){
+            caffeineView.setText("Today's caffeine consumption: " + modifyJSON(caffeine) + " mg!");
+        } else {
+            caffeineView.setText("Calculate your caffeine consumption on caffeine control!");
         }
 
         setBMI();
@@ -102,13 +116,13 @@ public class MainPage extends AppCompatActivity {
         DecimalFormat df = new DecimalFormat("0.00");
 
         if (BMI < 18.5){
-            bmiTextbox.setText("BMI: " + df.format(BMI) + " - Underweight");
+            bmiView.setText("BMI: " + df.format(BMI) + " - Underweight");
         } else if (BMI >= 18.5 && BMI < 24.9){
-            bmiTextbox.setText("BMI: " + df.format(BMI) + " - Normal weight");
+            bmiView.setText("BMI: " + df.format(BMI) + " - Normal weight");
         } else if (BMI >= 24.9 && BMI < 29.9){
-            bmiTextbox.setText("BMI: " + df.format(BMI) + " - Overweight");
+            bmiView.setText("BMI: " + df.format(BMI) + " - Overweight");
         } else if (BMI >= 29.9){
-            bmiTextbox.setText("BMI: " + df.format(BMI) + " - Obese");
+            bmiView.setText("BMI: " + df.format(BMI) + " - Obese");
         }
     }
 
